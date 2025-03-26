6c966013899215329329e91f859456a24667848c
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-03-26 21:22:09 +0300

    Completed lab

--- server/models.py
+++ server/models.py
@@ -11,22 +11,71 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # reviews relationship
+    reviews = db.relationship(
+        'Review', back_populates="customer"
+    )
+
+    # association proxy
+    items = association_proxy(
+        'reviews', 'item',
+        creator=lambda item_obj: Review(item=item_obj)
+    )
+
+    # serialization rules
+    serialize_rules = ('-reviews.customer',)
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # review relationship
+    reviews = db.relationship(
+        'Review', back_populates="item"
+    )
+
+    # serialization rules
+    serialize_rules = ('-reviews.item',)
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    # foreign keys
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    # relationship called customer
+    customer = db.relationship(
+        'Customer', back_populates='reviews'
+    )
+
+    # item relationship
+    item = db.relationship(
+        'Item', back_populates='reviews'
+    )
+
+    # serialization rules
+    serialize_rules = ('-customer.reviews', '-item.reviews',)
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}, {self.customer_id}, {self.item_id}>'
+
